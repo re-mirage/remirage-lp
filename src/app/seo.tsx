a46e4eec8fb5ff2ec911753c659f7af18ae698d4
Amcinox <amcinox@gmail.com>
2024-06-23 18:46:40 +0900

    Add Seo and sitemap

---
+++ src/app/seo.tsx
@@ -0,0 +1,31 @@
+import siteMetadata from '@/config/siteMetadata';
+import { Metadata } from 'next';
+
+interface PageSEOProps {
+  title: string;
+  description?: string;
+  image?: string;
+  [key: string]: any;
+}
+
+export function genPageMetadata({ title, description, image, ...rest }: PageSEOProps): Metadata {
+  return {
+    title,
+    description: description || siteMetadata.description,
+    openGraph: {
+      title: `${title} | ${siteMetadata.title}`,
+      description: description || siteMetadata.description,
+      url: './',
+      siteName: siteMetadata.title,
+      images: image ? [image] : [siteMetadata.socialBanner],
+      locale: 'en_US',
+      type: 'website',
+    },
+    twitter: {
+      title: `${title} | ${siteMetadata.title}`,
+      card: 'summary_large_image',
+      images: image ? [image] : [siteMetadata.socialBanner],
+    },
+    ...rest,
+  };
+}
