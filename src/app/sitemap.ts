e861251701abf262a57f4c455e47d4813d2abc1d
Amcinox <amcinox@gmail.com>
2024-07-15 13:31:00 +0900

    feat: Add dynamic routes for blog posts in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,22 @@
 import siteMetadata from '@/config/siteMetadata';
+import { Post } from '@/types/blog';
 import { MetadataRoute } from 'next';
+import { posts } from '@/mock/blog';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const siteUrl = siteMetadata.siteUrl;
-  const routes = ['', 'blog', 'contact', 'pricing', 'ressources', 'solution'].map((route) => ({
+  const routes = ['blog', 'contact', 'pricing', 'ressources', 'solution'].map((route) => ({
     url: `${siteUrl}/${route}`,
     lastModified: new Date().toISOString().split('T')[0],
     changeFrequency: 'weekly' as 'weekly',
   }));
+  posts.forEach((post: Post) => {
+    routes.push({
+      url: `${siteUrl}/blog/${post.slug}`,
+      lastModified: post.date,
+      changeFrequency: 'weekly',
+    });
+  });
 
   return [...routes];
 }
