477fe8c2543ea5e79de065fccadf362da59d8950
Amcinox <amcinox@gmail.com>
2024-07-21 22:28:25 +0900

    refactor: Remove BackgroundBeams component from ProjectsPage

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import projects from '@/mock/projects';
 import { ProjectsList } from '@/sections/projects/ProjectsList';
-import { BackgroundBeams } from '@/components/ui/background-beams';
 
 export default async function ProjectsPage({
   params,
