cf350b7fd302fb687ced26cd6bf12ba85a137d29
Amcinox <amcinox@gmail.com>
2024-07-21 15:22:45 +0900

    add types

---
+++ src/types/team.ts
@@ -0,0 +1,10 @@
+export interface Member {
+  username: string;
+  firstName: string;
+  lastName: string;
+  role: string;
+  avatar: string;
+  skills: string[];
+  bio: string;
+  projects: string[];
+}
