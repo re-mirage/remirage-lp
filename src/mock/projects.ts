3623dee436b3730a5b82da77ff30418d7cb27c30
Amcinox <amcinox@gmail.com>
2024-07-21 15:23:04 +0900

    add mock

---
+++ src/mock/projects.ts
@@ -0,0 +1,18 @@
+import { Project } from '@/types/project';
+
+const projects: Project[] = [
+  {
+    id: '1',
+    name: 'E-commerce Platform',
+    description: 'Built a scalable e-commerce platform using React and Node.js',
+    features: ['Natural Language Processing', 'Task Automation', '24/7 Availability'],
+  },
+  {
+    id: '2',
+    name: 'AI Chatbot',
+    description: 'Developed an AI-powered chatbot using natural language processing',
+    features: ['Natural Language Processing', 'Task Automation', '24/7 Availability'],
+  },
+];
+
+export default projects;
---
+++ src/mock/team.ts
@@ -0,0 +1,46 @@
+import { Member } from '@/types/team';
+
+const team: Member[] = [
+  {
+    username: 'simo',
+    firstName: 'MOHAMED',
+    lastName: 'EL BSSIR',
+    role: 'Senior Developer',
+    avatar: '/team/simo.jpg',
+    skills: ['React', 'Node.js', 'TypeScript', 'NextJs', 'AWS'],
+    bio: ' Mohamed is a seasoned developer with over 10 years of experience in web development...',
+    projects: ['1', '2'],
+  },
+  {
+    username: 'brahim',
+    firstName: 'Brahim',
+    lastName: 'EL Mimouni',
+    role: 'Senior Developer',
+    avatar: '/team/brahim.jpg',
+    skills: ['React', 'Node.js', 'TypeScript', 'NextJs', 'AWS'],
+    bio: 'Brahim is a seasoned developer with over 10 years of experience in web development...',
+    projects: ['1', '2'],
+  },
+  {
+    username: 'abdou',
+    firstName: 'Abderahim',
+    lastName: 'Abouslam',
+    role: 'Marketing Manager',
+    avatar: '/team/abdou.jpg',
+    skills: ['Facebook Ads', 'Google Ads', 'SEO', 'Content Marketing'],
+    bio: ' abdou is a marketing manager with over 10 years of experience in web development...',
+    projects: ['1', '2'],
+  },
+  {
+    username: 'walid',
+    firstName: 'Walid',
+    lastName: 'AR',
+    role: 'Manager',
+    avatar: '/team/walid.jpg',
+    skills: ['React', 'Node.js', 'TypeScript', 'NextJs', 'AWS'],
+    bio: 'Walid is a seasoned developer with over 10 years of experience in web development...',
+    projects: ['1', '2'],
+  },
+];
+
+export { team };
