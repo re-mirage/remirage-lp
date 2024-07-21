c1795d1c7bc247a39224a93bda94d2a84dada743
Amcinox <amcinox@gmail.com>
2024-07-21 15:24:39 +0900

    improve container component

--- src/layout/Container.tsx
+++ src/layout/Container.tsx
@@ -1,8 +1,13 @@
+import { cn } from '@/lib/utils';
 import React from 'react';
 
-interface ContainerProps {
+interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
 }
-export default function Container({ children }: ContainerProps) {
-  return <main className="flex flex-col items-center justify-between  ">{children}</main>;
+export default function Container({ children, className, ...props }: ContainerProps) {
+  return (
+    <main className={cn('flex flex-col items-center justify-between', className)} {...props}>
+      {children}
+    </main>
+  );
 }
