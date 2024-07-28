684c1393e117fd7c5de56916c253c71029770bb6
Amcinox <amcinox@gmail.com>
2024-07-28 21:41:13 +0900

    refactor Auth session

--- src/auth.ts
+++ src/auth.ts
@@ -1,59 +1,112 @@
-import NextAuth, { User, NextAuthConfig } from 'next-auth';
+import NextAuth, { NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { paths, ROOTS } from './routes/paths';
+import crypto from 'crypto';
 
-const username = 'demo@demo.com';
-const password = 'demo';
+interface CustomUser {
+  username: string;
+  email: string;
+  name: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+  skills: string[];
+  bio: string;
+  rate: number;
+  created_at: Date;
+  updated_at: Date;
+}
+
+declare module 'next-auth' {
+  interface Session {
+    user: CustomUser;
+  }
+}
+
+const mockDatabase = [
+  {
+    username: 'demo@demo.com',
+    email: 'demo@demo.com',
+    first_name: 'Mohamed',
+    last_name: 'EL BSSIR',
+    name: 'Remirage',
+    avatar: 'https://github.com/amcinox.png',
+    skills: ['NextJS', 'React'],
+    bio: 'Example',
+    rate: 20,
+    salt: 'fK8xP3mQ9r',
+    password: 'f9db03902050165d8fad61c12a0cf209f8281ee09dddde33938d6f94450f8978',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+];
+
+function hashPassword(password: string, salt: string): string {
+  const hash = crypto.createHmac('sha256', salt);
+  hash.update(password);
+  return hash.digest('hex');
+}
+
 const authConfig: NextAuthConfig = {
   trustHost: true,
   providers: [
     Credentials({
       credentials: {
-        username: { type: 'text' },
+        email: { type: 'text' },
         password: { type: 'password' },
       },
-      authorize: async (credentials): Promise<User | null> => {
-        if (!credentials?.username || !credentials?.password) {
-          throw new Error('Username and password are required');
+      authorize: async (credentials): Promise<Omit<CustomUser, 'salt' | 'password'> | null> => {
+        console.log({ credentials });
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Email and password are required');
         }
 
-        try {
-          if (credentials.username !== username || credentials.password !== password) {
-            console.log('here');
-            throw new Error('Incorrect password');
-          }
-          const user: User = {
-            email: username,
-            name: 'Remirage',
-            image: 'https://github.com/amcinox.png',
-          };
+        const user = mockDatabase.find((u) => u.email === credentials.email);
 
-          return user;
-        } catch (e) {
-          console.log({ e });
-          throw new Error('Invalid username or password');
+        if (!user) {
+          throw new Error('User does not exist');
         }
+
+        const hashedInputPassword = hashPassword(credentials.password as string, user.salt);
+        console.log({ hashedInputPassword });
+
+        if (hashedInputPassword !== user.password) {
+          throw new Error('Incorrect password');
+        }
+
+        const { salt, password, ...safeUser } = user;
+        return safeUser;
       },
     }),
   ],
   pages: {
     signIn: paths.auth.login,
   },
-  callbacks: {},
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.user = user as CustomUser;
+      }
+      return token;
+    },
+    session: async ({ session, token }) => {
+      session.user = token.user as any;
+      return session;
+    },
+  },
 };
 
 export const { handlers, signIn, signOut, auth } = NextAuth(authConfig);
 
 export async function authMiddleware(request: NextRequest) {
   const session = await auth();
-  console.log({ session });
   const url = request.nextUrl;
   const isAuthPage = url.pathname.startsWith(ROOTS.AUTH);
-  const isAdmingPage = url.pathname.startsWith(ROOTS.DASHBOARD);
+  const isAdminPage = url.pathname.startsWith(ROOTS.DASHBOARD);
 
-  if (isAdmingPage) {
+  if (isAdminPage) {
     if (!session) {
       return NextResponse.redirect(new URL(paths.auth.login, request.url));
     }
