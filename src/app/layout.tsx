c85c89de263346e9d01859edbdff65b3ab08b201
Amcinox <amcinox@gmail.com>
2024-09-01 15:14:16 +0900

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import siteMetadata from '@/config/siteMetadata';
 import Script from 'next/script';
+import { GoogleAnalytics } from '@next/third-parties/google'
+
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -55,17 +58,19 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-X9TDYNDM0P" />
-
         <Script id="google-analytics">
           {`
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-      gtag('config', ${'${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'});
+      gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
   `}
         </Script>
       </head>
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-X9TDYNDM0P" />
+
+
     </html>
   );
 }
